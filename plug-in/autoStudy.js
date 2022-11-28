456b1f13ee43d810aa9002f69c904322e9831b80
zouxianyu <2979121738@qq.com>
2022-11-28 22:27:10 +0800

    Update autoStudy.js
    Automatically skip the modal suspend window.

--- plug-in/autoStudy.js
+++ plug-in/autoStudy.js
@@ -16,6 +16,11 @@
 
     console.log("autoStudy begin!!!");
     window.setInterval(function () {
+        if($("#alertModal").css("display") != 'none' && $(".modal-info .title").text() == '计时学习已暂停') {
+            $(".modal-operation .btn-submit").click();
+            $(".mejs__overlay-button").click();
+            console.log("继续学习！");
+        }
         if($(".mejs__overlay-button").attr("aria-pressed")=='false') {
             $(".mejs__overlay-button").click();
         }
